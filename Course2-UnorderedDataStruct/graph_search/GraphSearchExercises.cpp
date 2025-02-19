785c5f5b18a98afa1f840c898abae0fd476bffcb
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-02-19 19:03:20 +0000

    finished exercise 3 puzzleBFS

--- Course2-UnorderedDataStruct/graph_search/GraphSearchExercises.cpp
+++ Course2-UnorderedDataStruct/graph_search/GraphSearchExercises.cpp
@@ -551,7 +551,7 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
     // We'll need to loop over the neighbors that are the points adjacent to curState.
     // We need a collection of neighbors we're going to loop over.
     
-    auto neighbors = {start}; // Change this! This line is totally wrong.
+    auto neighbors = curState.getAdjacentStates(); // Change this! This line is totally wrong.
 
     // Hint: Look at PuzzleState.h
     // =====================================================================
@@ -561,7 +561,11 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
       // ==================================================================
       // TODO: Your code here!
       // Check whether the neighbor has already been visited.
-      bool neighborWasAlreadyVisited = false; // Change this...
+      bool neighborWasAlreadyVisited = false;
+      if (visitedSet.count(neighbor)) 
+      {
+        neighborWasAlreadyVisited = true;
+      }
       // ==================================================================
 
       if (!neighborWasAlreadyVisited) {
@@ -573,13 +577,13 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
         // since curState has just led to the discovery of this neighbor for
         // the first time.
         // ...
-
+        pred[neighbor] = curState;
         // Add neighbor to the visited set.
         // ...
-
+        visitedSet.insert(neighbor);
         // Push neighbor into the exploration queue.
         // ...
-
+        exploreQ.push(neighbor);
         // ================================================================
 
         dist[neighbor] = dist[curState]+1;
--- Course2-UnorderedDataStruct/graph_search/main.cpp
+++ Course2-UnorderedDataStruct/graph_search/main.cpp
@@ -89,7 +89,7 @@ void informalTests() {
   // (The output will be very verbose.)
   constexpr bool doEx1Tests = false;
   constexpr bool doEx2Tests = false;
-  constexpr bool doEx3Tests = true;
+  constexpr bool doEx3Tests = false;
   // Some of the tests may take a few seconds to process. If that is annoying,
   // you can temporarily disable them by setting this to false:
   constexpr bool doEx3SlowTests = true; // requires doEx3Tests == true
